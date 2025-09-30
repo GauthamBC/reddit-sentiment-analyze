e3939e2a77412ff9c3d4e5c44501cffe57bbabdb
GauthamBC <gmarthandan@bettercollective.com>
2025-09-30 16:55:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ if uploaded_file:
     # ==============================
     # Buttons side by side
     # ==============================
-    col1, col2 = st.columns([1, 1])
+    col1, col2 = st.columns(2, gap="small")
     with col1:
         run_sentiment = st.button("🚀 Run Sentiment Analysis")
     with col2:
